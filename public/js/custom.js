ca575fb61fdfd67411d591d77c6ed78bdc3d6a29
Khriztian Moreno <khriztianmoreno@gmail.com>
2014-10-22 11:38:55 -0500

    the add script Google Analytics and submit contact form

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,1 +1,10 @@
 /* Add here all your JS customizations */
+
+var App = {
+    init: function(){        
+        this.myRootRef = new Firebase('https://web-site-brainswitch.firebaseio.com/');
+    },
+    save: function (item) {
+        this.myRootRef.child("users").child(item.Id).set(item);   
+    }
+};           
